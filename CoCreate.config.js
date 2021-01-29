1647b941918c38ccdec1f67b932537ec2867a2ad
frankpagan <frank@cocreate.app>
2021-01-29 01:19:29 +0000

    test doc

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "6013623c86c0ba24512cca40",
             key: "html",
             data:{
                 name: "Quill Adder Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "6013623c86c0ba24512cca3f",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "6013623c86c0ba24512cca40",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/quill-adder",
