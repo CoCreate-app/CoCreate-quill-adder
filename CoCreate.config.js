15daaaa819ece4186eccbddf5d11fbf70092fdea
frank <frank@cocreate.app>
2021-04-30 04:26:10 +0000

    add src

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "path": "./docs/index.html",
             "collection": "files",
             "document_id": "6013623c86c0ba24512cca40",
-            "key": "html",
+            "key": "src",
             "data":{
                 "name": "quill-adder Doc",
                 "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
